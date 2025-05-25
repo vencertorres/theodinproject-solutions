91e3603c415a113f39693b39b9ae9d1892e89b98
Vencer Torres <vencer.torres@gmail.com>
2025-05-25 09:30:42 +0800

    Remove unused parameter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,5 @@
 def bubble_sort(numbers)
-  numbers.length.times do |_|
+  numbers.length.times do
     (1...numbers.length).each do |i|
       if numbers[i - 1] > numbers[i]
         numbers[i - 1], numbers[i] = numbers[i], numbers[i - 1]
